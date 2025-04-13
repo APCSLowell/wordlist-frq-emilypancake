31b3997582c59a4f2093548aaacb155c9a463cc1
Emily Pan <pancakerula@gmail.com>
2025-04-12 17:33:17 -0700

    removed ) to fix syntax error

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,7 +11,7 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     int n = 0;
-    for(int i = myList.size(); i >= 0 ); i--){
+    for(int i = myList.size(); i >= 0; i--){
       if(myList.get(i).length() == len){
         n++;
       }
